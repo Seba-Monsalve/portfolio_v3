852c7941ae5b8e7e9391b523e032d6a879d7e792
Seba Monsalve <sebastian.monsalve@usach.cl>
2024-09-10 14:13:54 -0300

    asd

--- src/app/components/FormationInfo/FormationInfo.tsx
+++ src/app/components/FormationInfo/FormationInfo.tsx
@@ -10,7 +10,7 @@ export const FormationInfo = () => {
     <div className=' items-start mt-5 transl '>
       <Title>Experiencia</Title>
       <FormationItem cn=' rotate-2'  year={`Agosto 2020 - Actualidad (${TimeRelative('08-09-2020')})`} icon='icon' workplace='Emerix' title='Programador' />
-      <FormationItem cn='-translate-y-3 -rotate-2 bg-blue-400 ' year={`Marzo 2024 - Julio 2024`} icon='icon' workplace='Alura Latam' title='Estudiante Front-end' />
+      <FormationItem cn='-translate-y-3 -rotate-2 bg-blue-300 ' year={`Marzo 2024 - Julio 2024`} icon='icon' workplace='Alura Latam' title='Estudiante Front-end' />
       <FormationItem cn='-translate-y-3  rotate-3 bg-purple-500' year={`Enero 2024 - Actualidad`} icon='icon' workplace='DevTalles' title='React Roadmap' />
     </div>
   )
